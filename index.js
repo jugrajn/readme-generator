0f19b1dbc227d5cdc14421dff6068de7c1ec4839
jugrajn <jugraj.nijjar@gmail.com>
2021-06-10 21:57:55 -0400

    installed inquirer, created questions array, captured modules

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: 'input',
+       message: 'What is the title of your project?',
+       name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Provide a description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installInstruct',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'uInfo',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'guidelines',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions',
+        name: 'testInstruct',
+    },
+    {
+        type: 'list',
+        message: 'What is the license?',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'GPLv2', 'GPLv3', 'Mozilla Public 2.0', 'Other'],
+    },
+    {
+        type: 'input',
+        message: 'Please provide your GitHub username.',
+        name: 'userName',
+    },
+    {
+        type: 'input',
+        message: 'Please provide your email address',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
