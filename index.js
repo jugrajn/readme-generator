445ff414c6221ee2ba8da0a0cdf3f8156fd22096
jugrajn <jugraj.nijjar@gmail.com>
2021-06-11 17:41:13 -0400

    updated initialize function

--- index.js
+++ index.js
@@ -54,10 +54,23 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : null
+        );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            return
+        });
+        generateMarkdown
+            .then((data) => {
+                writeToFile(readme.md, data);
+            });
+}   
 // Function call to initialize app
 init();
